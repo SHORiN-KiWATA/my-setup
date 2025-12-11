e2f34a92eb970af8e77381f640ab6100010f3009
shorinkiwata <fcl709@outlook.com>
2025-12-11 21:22:23 +0900

    1

--- scripts/50-desktop-environment.sh
+++ scripts/50-desktop-environment.sh
@@ -11,3 +11,41 @@ PROGRESS_NAME="install_desktop_environment"
 create_xdg_user_dirs(){
         pacman -Syu --noconfirm xdg-user-dirs
 }
+
+get_applist(){
+        local applist_path="$1"
+        local pacman_list=""
+        local aur_list=""
+        local flatpak_list=""
+        # read循环读取done后面导入的文件放入line变量，-r原样读取，不转义。
+        log_info "Processing pkg list...."
+        while read -r line; do
+                #如果是注释就跳过
+                [[ "$line" =~ ^# ]] && continue
+                #如果是空行就跳过 
+                [[ -z "$line" ]] && continue
+
+                # 如果包是AUR：开头的（这里也是正则匹配，== AUR：通配符）
+                if [[ "$line" == AUR:* ]]; then
+                        # var+=(value)意思是追加赋值，line#AUR:意思是把变量值里开头的AUR：去掉
+                        aur_list+=("${line#AUR:}")
+                elif [[  "$line" == flatpak:* ]]; then
+                        flatpak_list+=("${line#flatpak:}")
+                else
+                        pacman_list+=("$line")
+                fi
+        done < "$applist_path"
+        log_info "Pkg list complete; installing pkgs...."
+        if [ -z "$pacman_list" ]; then 
+                #将数组变量里的每一个值逐个传给pacman，类似"vim" "firefox" "mission-center"这样
+                pacman -Syu --needed --noconfirm "${pacman_list[@]}"
+        fi
+        if [ -z "$aur_list" ]; then 
+                yay -Syu --needed --noconfirm --noanswerclean --noansweredit --noanswerdiff --noanswerupgrade "${aur_list[@]}"
+        fi
+        if [ -z "$flatpak_list" ]; then
+                flatpak install "${flatpak[@]}"
+        fi
+}
+
+get_applist
