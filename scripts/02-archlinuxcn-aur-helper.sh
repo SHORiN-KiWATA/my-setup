5f98c7213d1ee61fafe0ee2bdeb1d91d8764e0a5
shorinkiwata <fcl709@outlook.com>
2025-12-11 20:37:22 +0900

    1

--- scripts/02-archlinuxcn-aur-helper.sh
+++ scripts/02-archlinuxcn-aur-helper.sh
@@ -16,12 +16,12 @@ if ! if_is_complete; then
                 #告诉shell接下来遇到EOT（end of text）前的所有内容都追加写入/etc/pacman.conf
                 cat <<EOT >> /etc/pacman.conf
 
-        [archlinuxcn]
-        Server = https://mirrors.ustc.edu.cn/archlinuxcn/\$arch
-        Server = https://mirrors.tuna.tsinghua.edu.cn/archlinuxcn/\$arch
-        Server = https://mirrors.hit.edu.cn/archlinuxcn/\$arch
-        Server = https://repo.huaweicloud.com/archlinuxcn/\$arch 
-        EOT
+[archlinuxcn]
+Server = https://mirrors.ustc.edu.cn/archlinuxcn/\$arch
+Server = https://mirrors.tuna.tsinghua.edu.cn/archlinuxcn/\$arch
+Server = https://mirrors.hit.edu.cn/archlinuxcn/\$arch
+Server = https://repo.huaweicloud.com/archlinuxcn/\$arch 
+EOT
         
                 pacman -Sy --noconfirm archlinuxcn-keyring 
                 if ! has_cmd "yay"; then
